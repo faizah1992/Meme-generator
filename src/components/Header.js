665038987484bb99460b2a6a6c5be96f282f6b74
Faizah Ahsan <faizah1992@gmail.com>
2022-03-02 11:06:00 -0600

    style added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import trollFace from "../images/Troll Face.png"
 export default function Header(){
     return(
         <header className="header">
-            <img src={trollFace} alt="troll face"/>
-            <h2>
+            <img src={trollFace} alt="troll face" className="header---img"/>
+            <h2 className="header---title">
                 Meme Generator
             </h2>
-            <h4>
+            <h4 className="header---project">
                 React Course - Project 3
             </h4>
         </header>
